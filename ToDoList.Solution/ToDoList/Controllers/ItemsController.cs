409663c03aba4a59f493d0013ed3577f85ecede8
Krista Rutz <rutzkri000@gmail.com>
2020-03-18 09:43:22 -0700

    make items part of categories
    Co-authored-by: Tiffany Siu <tsiu88@gmail.com>

--- ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
+++ ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
@@ -3,6 +3,7 @@ using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -17,12 +18,13 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList(); // ToList() will give us access to all our Items in List
+      List<Item> model = _db.Items.Include(items => items.Category).ToList();
       return View(model);
     }
 
     public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
 
@@ -44,6 +46,7 @@ namespace ToDoList.Controllers
     public ActionResult Edit(int id)
     {
       var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View(thisItem);
     }
 
